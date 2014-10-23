ad1a6ec7c1124a9997f8051ed00d500dd4bb74f9
abs <abs.pathak@ticketmaster.com>
2014-10-23 15:20:31 -0700

    Cleanup

--- README.rst
+++ README.rst
@@ -8,3 +8,20 @@ Currently the following data is collected:
 * Metrics
 * Tag keys
 * Tag values
+
+
+Build
+-----
+::
+	$ mkdir -p MetrilyxCacher/{src,bin,pkg}
+	$ cd MetrilyxCacher
+	$ export GOPATH=$(pwd)
+
+	$ go get github.com/euforia/metrilyx-cacher
+	$ go install github.com/euforia/metrilyx-cacher
+
+This will produce a binary under **bin/metrilyx-cacher**
+
+Installation
+------------
+Copy the generated binary to the desired location.
--- metrilyx-cacher.go
+++ metrilyx-cacher.go
@@ -5,6 +5,7 @@ import(
     "fmt"
     "log"
     "flag"
+    "io/ioutil"
     "net/http"
     "time"
     "encoding/json"
@@ -14,28 +15,75 @@ import(
 /* endpoint to tsdb name mapping */
 var MetaMap = map[string]string{"metrics": "metrics", "tagnames": "tagk", "tagvalues": "tagv"}
 
+type TSDBDataprovider struct {
+    URI string            `json:"uri"`
+    Port int              `json:"port"`
+    SearchEndpoint string `json:"search_endpoint"`
+}
+
+type DataproviderConfig struct {
+    Dataprovider TSDBDataprovider `json:"dataprovider"`
+}
+
+func getTsdbUrl(cfgfile string) string {
+        file, e := ioutil.ReadFile(cfgfile)
+        if e != nil {
+            fmt.Printf("File error: %v\n", e)
+            os.Exit(1)
+        }
+        var dpconfig DataproviderConfig
+        json.Unmarshal(file, &dpconfig)
+        
+        return fmt.Sprintf("%s:%d%s", 
+                        dpconfig.Dataprovider.URI, 
+                        dpconfig.Dataprovider.Port, 
+                        dpconfig.Dataprovider.SearchEndpoint)
+}
+
+func printHelp() {
+    fmt.Printf("\n Usage:\n\n")
+    flag.PrintDefaults()
+    fmt.Println()
+}
+
 func initFlags() (string, string, string, int) {
     var listenAddr string
     var tsdbUrl string
     var endpoint string = "/"
     var refreshInterval int
-    //var configFile string
+    var configFile string
 
     flag.StringVar(&listenAddr, "listen-addr", ":8989", "HTTP Server Port")
     flag.IntVar(&refreshInterval, "refresh-interval", 180, "Cache refresh in seconds")
     //flag.StringVar(&endpoint, "endpoint", "/", "Endpoint prefix to serve data")
     flag.StringVar(&tsdbUrl, "url", "", "Suggest URL endpoint to OpenTSDB (e.g. http://localhost:4242/api/suggest)")
-    //flag.StringVar(&configFile, "-config", "metrilyx-cacher.json", "Configuration file")
+    flag.StringVar(&configFile, "config", "", "Configuration file")
     flag.Parse()
     
-    if tsdbUrl == "" {
-        fmt.Printf("OpenTSDB url required!")
-        fmt.Printf("\n Usage:\n\n")
-        flag.PrintDefaults()
-        fmt.Println()
-        os.Exit(1)
+    if configFile != "" {
+        tsdbUrl = getTsdbUrl(configFile)
+    } else {
+        if tsdbUrl == "" {
+            fmt.Printf("OpenTSDB url required!")
+            printHelp()
+            os.Exit(1)
+        }
     }
     return listenAddr, tsdbUrl, endpoint, refreshInterval
+}
+
+func writeHttpResponse(writer http.ResponseWriter, data []byte, respCode int) int {
+    writer.Header().Set("Access-Control-Allow-Origin", "*")
+    writer.Header().Set("Content-Type", "application/json")
+    writer.WriteHeader(respCode)
+    writer.Write(data)
+    return respCode
+}
+
+func writeHttpOptionsResponse(writer http.ResponseWriter) int {
+    writer.Header().Set("Access-Control-Allow-Origin", "*")
+    writer.WriteHeader(200)
+    return 200
 }
 
 func main() {
@@ -48,7 +96,7 @@ func main() {
         mcache = opentsdb.FetchMetadata(TSDB_URL)
     }()
 
-    log.Printf("Setting caching schedule: %d...\n", REFRESH_INTERVAL)
+    log.Printf("Setting caching schedule: %d secs...\n", REFRESH_INTERVAL)
     ticker := time.NewTicker(time.Duration(REFRESH_INTERVAL) * time.Second)
     quit := make(chan struct{})
     go func() {
@@ -66,29 +114,32 @@ func main() {
     }()
 
     http.HandleFunc(SERVE_ENDPOINT, func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("%s %s %s\n", r.Method, r.RequestURI, r.RemoteAddr)
-
+        //log.Printf("%s %s %s\n", r.Method, r.RequestURI, r.RemoteAddr)
         metadataType := r.URL.Path[1:]
-        _, ok := MetaMap[metadataType]
-        if !ok {
-            w.WriteHeader(404)
-            return
-        }
-        params := r.URL.Query()
-        if val,ok := params["q"]; ok {
-            results := mcache.SearchByType(MetaMap[metadataType], val[0])
-            bytes, err := json.Marshal(results)
-            if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
+        var respCode int
+
+        if r.Method == "OPTIONS" {
+            respCode = writeHttpOptionsResponse(w)
+        }  else {
+            _, ok := MetaMap[metadataType]
+            if !ok {
+                respCode = writeHttpResponse(w, []byte(`{"error": "page not found"}`), 404)
+            } else {
+                params := r.URL.Query()
+                if val,ok := params["q"]; ok {
+                    results := mcache.SearchByType(MetaMap[metadataType], val[0])
+                    bytes, err := json.Marshal(results)
+                    if err != nil {
+                        http.Error(w, err.Error(), http.StatusInternalServerError)
+                        return
+                    }
+                    respCode = writeHttpResponse(w, bytes, 200)
+                } else {
+                    respCode = writeHttpResponse(w, []byte(`{"error": "'q' param required"}`), 500)
+                }
             }
-            w.Header().Set("Content-Type", "application/json")
-            w.Write(bytes)
-        } else {
-            w.Header().Set("Content-Type", "application/json")
-            w.WriteHeader(500)
-            w.Write([]byte(`{"error": "'q' param required"}`))
         }
+        log.Printf("%s %d %s %s\n", r.Method, respCode, r.RequestURI, r.RemoteAddr)
     })
 
     log.Printf("Starting server %s%s", LISTEN_ADDR, SERVE_ENDPOINT)
